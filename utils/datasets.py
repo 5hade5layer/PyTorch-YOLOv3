6aa238c0195f9354261061697f1a24afd3f4e1f6
5hade5layer <49690305+5hade5layer@users.noreply.github.com>
2019-07-05 13:48:39 +0530

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -60,7 +60,7 @@ class ListDataset(Dataset):
     def __init__(self, list_path, img_size=416, augment=True, multiscale=True, normalized_labels=True):
         with open(list_path, "r") as file:
             self.img_files = file.readlines()
-
+        self.img_files=random.sample(self.img_files,4000)
         self.label_files = [
             path.replace("images", "labels").replace(".png", ".txt").replace(".jpg", ".txt")
             for path in self.img_files
